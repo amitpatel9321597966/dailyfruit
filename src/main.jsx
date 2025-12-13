9ed3c6fd471b52e0cbc03341a34c927d9073b307
Amit Patel <amitpatel9321597966@gmail.com>
2025-12-14 00:17:05 +0530

    fix cartcontext import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-//import CartProvider from "./context/CartContext";//
+import CartProvider from "./context/cartcontext";
 import "./index.css";
-import CartProvider, { CartContext } from "./context/cartcontext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
@@ -11,3 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </CartProvider>
   </BrowserRouter>
 );
+
+
+//import CartProvider from "./context/CartContext";//
